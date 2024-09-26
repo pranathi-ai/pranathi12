5340773199ec36235f17d7599646447e0351e6ef
pranathi-ai <pranathiakella123@gmail.com>
2024-09-26 10:52:42 +0530

    Update add.java

--- add.java
+++ add.java
@@ -2,7 +2,7 @@ class add
   {
     public static void main(String args[])
     {
-       System.out.println("devops")
-       System.out.println("contious integration & continous delivery ")
+       System.out.println("devops");
+       System.out.println("contious integration & continous delivery ");
          }
   }
