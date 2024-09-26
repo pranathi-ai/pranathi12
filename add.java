136df25c303b9d802420d504a391d9a9935cd0ba
pranathi-ai <pranathiakella123@gmail.com>
2024-09-26 10:52:07 +0530

    Create add.java

---
+++ add.java
@@ -0,0 +1,8 @@
+class add
+  {
+    public static void main(String args[])
+    {
+       System.out.println("devops")
+       System.out.println("contious integration & continous delivery ")
+         }
+  }
